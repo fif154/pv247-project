a4ecb6a6c4389fa32446315782757482d635ab36
Jan Glos <jan.glos@inqool.cz>
2025-05-08 16:54:31 +0200

    chore: remove file

--- src/utils/error.ts
+++
@@ -1,8 +0,0 @@
-export type ActionError = { error: string };
-export type ServerActionResponse<T = object> = ActionError | undefined | T;
-
-export function isActionError<T = object>(
-    response: ServerActionResponse<T>
-): response is ActionError {
-    return (response as ActionError).error !== undefined;
-}
