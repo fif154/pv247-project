a562fe2e9c7a3649bc000e4955cb689e89f19539
Jan Glos <jan.glos@inqool.cz>
2025-05-14 01:23:34 +0200

    fix: mobile view

--- src/components/meal-card-list.tsx
+++ src/components/meal-card-list.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { MealWithMacros } from '@/server/entities/models/meal';
+import { useRouter } from 'next/navigation';
 import { MealCard } from './meal-card';
 import { Button } from './ui/button';
-import { useRouter } from 'next/navigation';
 
 type MealCardListProps = {
   meals: MealWithMacros[];
@@ -20,7 +20,7 @@ export const MealCardList = ({ meals, mealType }: MealCardListProps) => {
         {meals.length === 0 ? (
           <Button
             variant="outline"
-            className="mt-4 w-1/4"
+            className="mt-4 md:w-1/4"
             onClick={() => router.push('/auth/meal-plans')}
           >
             {`Add ${mealType}`}
